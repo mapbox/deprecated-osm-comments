b81007ff8e9a1e95d58b94f809027560f561afe9
Sanjay Bhangar <sanjay@mapbox.com>
2015-12-04 18:44:07 +0530

    init Mapbox map correctly, related to #2

--- app/components/Map.js
+++ app/components/Map.js
@@ -5,17 +5,8 @@ import L from 'mapbox.js';
 var Map = React.createClass({
     componentDidMount: function() {
         var token = config.MAPBOX_ACCESS_TOKEN;
-        console.log('map component did mount');
-        console.log('L', L);
-        var map = L.map(this.refs.map)
-            .setView([40, -74.50], 9);
-        var tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox.streets',
-            accessToken: token
-        }).addTo(map);
-
+        L.mapbox.accessToken = token;
+        L.mapbox.map(this.refs.map, 'mapbox.streets');
     },
 
     render: function() {
