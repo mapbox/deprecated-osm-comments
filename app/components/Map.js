92fddcf3cbd21e587f58896238ecc1be8215f3de
Sanjay Bhangar <sanjay@mapbox.com>
2015-12-04 17:41:27 +0530

    initialize map, refs #2

--- app/components/Map.js
+++ app/components/Map.js
@@ -1,15 +1,26 @@
 import React, {Component} from 'react';
+import config from '../config';
+import L from 'leaflet';
 
 var Map = React.createClass({
     componentDidMount: function() {
+        var token = config.MAPBOX_ACCESS_TOKEN;
         console.log('map component did mount');
+        console.log('L', L);
+        var map = L.map(this.refs.map)
+            .setView([40, -74.50], 9);
+        var tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox.streets',
+            accessToken: token
+        }).addTo(map);
+
     },
 
     render: function() {
         return (
-            <div className="col6">
-                I am your map.
-            </div>
+            <div className="col6 map" ref="map" />
         );
     }
 });
--- app/config.js
+++ app/config.js
@@ -1,5 +1,6 @@
 const config = {
-    'API_BASE': 'http://localhost:8888/api/v1/'
+    'API_BASE': 'http://localhost:8888/api/v1/',
+    'MAPBOX_ACCESS_TOKEN': 'pk.eyJ1Ijoic2FuamF5YiIsImEiOiI3NjVvMFY0In0.byn_eCZGAwR1yaPeC-SVKw'
 };
 
 export default config;
