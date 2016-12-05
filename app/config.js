0759388e349762fa6ac5271bda971c7b9b90672e
Sanjay Bhangar <sanjay@mapbox.com>
2016-12-05 11:11:42 +0530

    switch to new end-point - osm-comments-api.mapbox.com

--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
 var mapboxDataTeam = require('mapbox-data-team');
 
 const config = {
-    'API_BASE': 'https://api-osm-comments-production.tilestream.net/api/v1/',
+    'API_BASE': 'https://osm-comments-api.mapbox.com/api/v1/',
     'MAPBOX_ACCESS_TOKEN': 'pk.eyJ1Ijoic2FuamF5YiIsImEiOiI3NjVvMFY0In0.byn_eCZGAwR1yaPeC-SVKw',
     'OSM_BASE': 'https://www.openstreetmap.org/',
     'STATIC_MAPS_BASE': 'https://api.mapbox.com/v4/mapbox.streets/',
